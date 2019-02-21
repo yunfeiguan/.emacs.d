124afa9ae2782939e6a383957272c8e42c0fbcbb
Adam C. Emerson <azure@fox.blue>
2019-02-21 11:21:15 -0500

    C and O'CaML fixes

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -69,7 +69,6 @@
 (setq rtags-completions-enabled t)
 (eval-after-load 'company
   '(add-to-list 'company-backends 'company-rtags))
-(setq rtags-autostart-diagnostics t)
 (rtags-enable-standard-keybindings)
 
 (provide 'init-c)
--- lisp/init-ocaml.el
+++ lisp/init-ocaml.el
@@ -8,9 +8,10 @@
 
 (require 'init-elpa)
 (require-package 'tuareg)
+(require-package 'merlin)
+(require-package 'flycheck-ocaml)
 (require 'tuareg)
 (require 'camldebug)
-
 
 (add-hook 'tuareg-mode-hook 'tuareg-imenu-set-imenu)
 
@@ -19,10 +20,15 @@
 		("\\.topml$" . tuareg-mode))
 	      auto-mode-alist))
 
-
-(when (require 'merlin '() t)
-  (add-hook 'tuareg-mode-hook 'merlin-mode)
-  (add-hook 'caml-mode-hook 'merlin-mode))
+(add-hook 'tuareg-mode-hook 'merlin-mode)
+(add-hook 'caml-mode-hook 'merlin-mode)
+(with-eval-after-load 'company
+  (add-to-list 'company-backends 'merlin-company-backend)
+  ;; Disable Merlin's own error checking
+  (setq merlin-error-after-save nil)
+  ;; Enable Flycheck checker
+  (flycheck-ocaml-setup))
+(add-hook 'merlin-mode-hook 'company-mode)
 
 (provide 'init-ocaml)
 ;;; init-ocaml.el ends here
